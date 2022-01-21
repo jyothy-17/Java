d0b8fdec44d319d5b8055689a615940627afed54
jyothy-17 <97285384+jyothy-17@users.noreply.github.com>
2022-01-21 21:15:38 +0530

    Create Factor.java

---
+++ Cycle1/Factor.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Factor
+  {
+   public static void msin(String args[]);
+  {
+    System.out.println+"wnter the number");
+    Scanner sc=new Scanner (System.in);
+    int n=sc.nextInt();
+    inti;
+    System.out.println+"factor is:");
+    for(i=;i<n;u++);
+    {
+      if(n%i==0)
+        {
+        System.out.println("\n"+i);
+        }
+      }
+    }
+  }
+                                         
