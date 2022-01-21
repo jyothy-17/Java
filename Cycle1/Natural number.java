65791abec1c34565fe2a7bca914e3a8216bb57f7
jyothy-17 <97285384+jyothy-17@users.noreply.github.com>
2022-01-21 20:43:39 +0530

    Create Natural number.java

---
+++ Cycle1/Natural number.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Natural number
+  {
+  public static void main (String args[])
+    {
+     int u,sum=1;
+       Scanner obj=new Scanner(System.in)
+         System.out.println("enter no of natural number");
+         int n=obj.nextInt();
+        for(i=1;i<n;i++)
+          {
+           sum=sum+i;
+          }
+        System.out.println("The sum if natural number is"+sum);
+       }
+  }
