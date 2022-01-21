2ca2316adc0b13500c1e079e8737af6429c61755
jyothy-17 <97285384+jyothy-17@users.noreply.github.com>
2022-01-21 19:47:58 +0530

    Create Sum.java

---
+++ Cycle1/Sum.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Sum
+ {
+  public static void main(String args[])
+    {
+      int a,b,sum;
+    System.out.println("enter the two number:\n");
+    Scanner obj=new Scanner(System.in);
+    a=obj.nextInt();
+    b=obj.nextInt();
+    sum=a+b;
+    System.iut.println("The sum is"+sum);
+    }
+  }
+    
