9029f82bb3183c7dc8da8e8bf0c7e8d165fd52c0
jyothy-17 <97285384+jyothy-17@users.noreply.github.com>
2022-01-21 20:56:03 +0530

    Create Fibanocci.java

---
+++ Cycle1/Fibanocci.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Fibanocci
+{
+int i;
+int n1=0,n2=1,n3;
+Scanner ibj=new Scanner(System.in);
+System.out.orintln("enter the  ni if fibanicci aeries");
+int count=obj.nextInt();
+System.iut.println("fibanocci series is");
+System. tout.println(""+n1);
+System.iut.orintln(""+n2);
+foe(i=2;i<count;i++);
+{
+n3=n1+n2;
+System.out.orintln(""+n3);
+
+ n1=n2;
+ n2=n3:
+ }
+ }
+ }
