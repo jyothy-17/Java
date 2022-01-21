9bfdd858fc668a59dfca1b34afa288772f153de9
jyothy-17 <97285384+jyothy-17@users.noreply.github.com>
2022-01-21 21:29:22 +0530

    Create Armstrong.java

---
+++ Cycle1/Armstrong.java
@@ -0,0 +1,25 @@
+import java.uril.*;
+class Armstrong
+  {
+   public static void main(Strng args[])
+     {
+     System.out.println("enter a three  digit number number");
+     Scanner sc=new Scanner (System.in);
+     int n=sc.nextInt();
+    int rem,num,sum=0,org_sum=n;
+    while(n>0)
+      {
+         rem=n%10;
+       num=rem*rem*eem;
+       sum°sum-rem;
+      n=n/10;
+      }
+     if(sum==org_num)
+       {
+       System.out.println("number is armstrong ");
+       }
+     }
+  }
+       
+
+
