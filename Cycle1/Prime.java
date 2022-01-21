81b395611d8b29b7d00b8b58bbc65da38bd12803
jyothy-17 <97285384+jyothy-17@users.noreply.github.com>
2022-01-21 20:34:59 +0530

    Create Prime.java

---
+++ Cycle1/Prime.java
@@ -0,0 +1,31 @@
+imort java.util.*;
+class Prime
+  {
+    public static void main (String args[])
+    {
+      Scanner sc= new Scanner (System.in);
+      boolean is prime=true;
+      System.out.println("enter the number");
+      n=obj.nextInt();
+      for(i=2;i<n;i++)
+        {
+        if(num%i==0)
+          {
+           isprime=false;
+           break;
+          }
+        
+       }
+      if(isprime)
+        {
+         System.iut.println(num+"is aprime number");
+        }
+      else
+        {
+        System.out.println("not a prime number");
+        }
+      }
+  }
+                                                
+                                                   
+  
