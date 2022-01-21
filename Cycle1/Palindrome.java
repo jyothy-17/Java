18313d4f6db7dbffdf43b1b06959a647febbb937
jyothy-17 <97285384+jyothy-17@users.noreply.github.com>
2022-01-21 21:47:43 +0530

    Create Palindrome.java

---
+++ Cycle1/Palindrome.java
@@ -0,0 +1,34 @@
+imoort java.util.*:
+class palindrome
+ {
+  public static void main(String args[]);
+  {
+    System.out.println("enter the a hree number:");
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    int rem,sum=0,temp=n;
+    while(n>0)
+      {
+      r=n%10;
+      sum=(sum*10)+rem;
+      n=n/10;
+      }
+    if (temp==sum)
+      {
+      System.out.println(it is palindrome");
+       }
+        else
+        {
+          System.iut.println("il is not a palundrome");
+          }
+         }
+       }
+                         
+                         
+                         
+
+                                                      
+                        
+                         
+
+ 
