0bfb7dfedf0a0edb77e24be3db60f3a245e59969
jyothy-17 <97285384+jyothy-17@users.noreply.github.com>
2022-01-21 20:07:57 +0530

    Create Odd.java

---
+++ Cycle1/Odd.java
@@ -0,0 +1,24 @@
+umport java.util.*;
+class Odd
+{
+public static void main (String args[])
+{
+     int n;
+     System.out.println("enter a number:");
+     Scanner obj=new Scanner(System.in(;
+     n=ibj.nextInt();
+     if(n%2==0)
+     {
+       System.out.println("it is ecen");
+       }
+       
+       else
+       {
+       System.out.println("it is odd");
+       }
+         
+         
+    }
+   }
+    
+ 
