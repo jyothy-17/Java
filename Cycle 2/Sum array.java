1337e37819970c9e2e6dc207868f63eb8ab9c603
jyothy-17 <97285384+jyothy-17@users.noreply.github.com>
2022-01-22 13:02:02 +0530

    Create Sum array.java

---
+++ Cycle 2/Sum array.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Sum array
+  {
+   public static void main(String args[]);
+    {
+       System.out.println("enter the number of element in array:");
+       Scanner sc=new Scanner (System.in);
+       int n=sc.nextInt();
+       int sum=0,i;
+        System.out.println("enter the  array element:");
+         for(i=0;i<n;i++)
+           {
+            a[i]=sc.nextInt();
+             }
+             for(i=0;i<n;i++)
+               {
+                sum=sum+a[i];
+               }
+          System.out.println("sum of array element is:");
+      }
+  }
