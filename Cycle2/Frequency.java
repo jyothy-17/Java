2ea6728a2a9ee10daa91b59b097b5fb66394f6cb
jyothy-17 <97285384+jyothy-17@users.noreply.github.com>
2022-01-22 21:53:37 +0530

    Create Frequency.java

---
+++ Cycle2/Frequency.java
@@ -0,0 +1,23 @@
+import.javs.util.*;
+class Frequency
+  {
+    public static void main (String args[])
+      {
+       Scanner sc=new Scanner (System.in)
+         System.out.println("enter the string":);
+         string sc=sc.next();
+        System.out.println("enter the  character whose frequency to be find:);
+         char ch=sc.next().charAt[0];
+          inti, j=s.length(),c=0;
+          for (i=0;i<j;i++)
+           {
+             char cha=s.charAt[i];
+             if(cha==ch)
+               {
+               c=c+1;
+               }
+             }
+             System.out.println("The character",+c);
+             
+              }
+             }
