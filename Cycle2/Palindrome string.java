fad4f2610a09140750a40a725365f8093d06a6dd
jyothy-17 <97285384+jyothy-17@users.noreply.github.com>
2022-01-22 20:56:47 +0530

    Create Palindrome string.java

---
+++ Cycle2/Palindrome string.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Palindrome string
+  {
+  int i,flag=0,c=0;
+  Scanner obj= new Scanner (System.in);
+  System .out.println(" enter the string lengrh");
+  int n=obj.nextInt();
+  System.out.println("enter the  string");
+  char a[]=new char[20];
+  for(i=0;a[i]!="\0";i++)
+    if( a[i]!=0[n_i_1])
+      {
+      flag=1;
+      break;
+      }
+    if(flag==1)
+      System.out.println(" string is not palindrome");
+    else
+      System.out.println(" string is a palindrome");
+      }
+}
