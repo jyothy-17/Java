e3e5cb34768d432610d35824b87d5cd0046dda87
jyothy-17 <97285384+jyothy-17@users.noreply.github.com>
2022-01-23 13:11:05 +0530

    Create Concatenated.java

---
+++ Cycle 3/Concatenated.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class Concatenated
+  {
+  public static void main (String args[])
+    {
+    Scanner obj=new Scanner (System.in);
+    System.out.println("enter the two string to be concatenated:");
+    String c=obj.nextLine();
+    String d=obj.nextLine();
+    System.out.print("enter the two number to be concatenated:");
+    int a=obj.nextInt();
+    int b=ibj.nextInt();
+    solve ibj1=new solve ();
+    obj1.Concatenated(a,b);
+    obj1.Concatenated(c,d);
+    }
+  }
+class Solve
+    {
+      void Concatenated(int a,int b)
+        {
+          System.out.println("concatenated number="+a+b);
+        }
+    void Concatenated(String a, String b)
+      {
+       System.out.println("Concatenated string="+a+b);
+      }
+  }
