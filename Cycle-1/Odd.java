d27bc46d046fcc665e5ba7d4e6f723becfc8488a
jyothy-17 <97285384+jyothy-17@users.noreply.github.com>
2022-01-23 15:21:52 +0530

    Create Odd.java

---
+++ Cycle-1/Odd.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Odd
+  {
+   public static void main(String args[])
+     
+     {
+      int n;
+     System.out.println("enter the number:");
+     Scanner obj=new Scanner (System.in);
+     n=obj.nextInt();
+     if(n%2==0)
+       {
+       System.out.println("it is even");
+       }
+     else
+       {
+       System.out.println("it is a odd");
+       }
+     }
+  }
+                          
+       
