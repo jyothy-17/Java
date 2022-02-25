19615cbb6e83403b30b125da41c1d104088f9ae6
jyothy-17 <97285384+jyothy-17@users.noreply.github.com>
2022-02-25 19:40:11 +0530

    Create Exp-1c.java

---
+++ Cycle6/Exp-1c.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class exp-1c
+{
+public static void main (String args[]) throws exception
+{
+ try
+ {
+ FileInputStream=new FileInputStream("e1.txt");
+ int i;
+  while(i=file.read()!=-1)
+  
+  {
+   System.out.println(char()i);
+   }
+   catch(filenotfoundException e)
+   {
+   System.out.println(e);
+   }
+   }
+   }
