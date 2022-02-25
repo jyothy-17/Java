0be30ecd5d6800a524c12a8d81befadb01514501
jyothy-17 <97285384+jyothy-17@users.noreply.github.com>
2022-02-25 19:25:21 +0530

    Create Exp-la.java

---
+++ Cycle6/Exp-la.java
@@ -0,0 +1,18 @@
+class exp-la
+{
+Public static void main (String args[])
+
+{
+ try
+ {
+  int a=10,b;
+  b=a/0;
+  System.out.println(b);
+  }
+  catch (Arithmetic Exception e)
+  {
+   System.out.println(e);
+   }
+   }
+   }
+   }
