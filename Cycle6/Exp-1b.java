723cd500d20ac9de2af12cc7196477fa7109c24f
jyothy-17 <97285384+jyothy-17@users.noreply.github.com>
2022-02-25 19:33:05 +0530

    Create Exp-1b.java

---
+++ Cycle6/Exp-1b.java
@@ -0,0 +1,15 @@
+class exp-lb
+{
+ public static void main (String args[])
+ {
+ try
+ {
+  int a={1,2,3,4,5);
+   System.out.println(a[8]);
+   
+   catch(ArrayIndexOutOfBoundException e)
+   {
+    System.out.ptintln(e);
+    }
+    }
+    }
