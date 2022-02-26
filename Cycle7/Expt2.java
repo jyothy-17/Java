ab0ca9cf49d659d53115c8446016b09914701953
jyothy-17 <97285384+jyothy-17@users.noreply.github.com>
2022-02-26 21:05:05 +0530

    Create Expt2.java

---
+++ Cycle7/Expt2.java
@@ -0,0 +1,23 @@
+class expt2
+  {
+  public static void main(String args[])
+    {
+    try
+      {
+      int b=100;
+      if(b<500)
+      {
+        throw one liw balanceExpection ();
+        }
+      }
+    catch low balance Exception extends Exception
+      {
+        
+         low balance Exception () 
+          
+         {
+           System.out.println("low balance");
+           }
+        }
+        
+      
