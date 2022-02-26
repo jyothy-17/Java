18b76d02088faf689a7a01631b5328fd0c3b809e
jyothy-17 <97285384+jyothy-17@users.noreply.github.com>
2022-02-26 20:28:58 +0530

    Create Bank.java

---
+++ Cycle7/Bank.java
@@ -0,0 +1,84 @@
+
+class Bank
+  {
+   public static void main (String args[])
+     {
+      Account a =new Account ();
+      Deposit d=new Deposit (d);
+      wthdraw w=new withdraw(w);
+      d.start();
+      w.start();
+     }
+  }
+       
+     class Account
+       {
+       int amount=500;
+       if(this .amount<amount)
+         
+         {
+         try
+           {
+           wait ();
+           }
+          catch(Exception e)
+            {
+            System.out.println(e);
+            }
+         }
+       this.amount=amount;
+       if(this.amount<amount)
+         {
+         System.out.println("withdraw completed");
+         System.out.println("available balance:"+this.amount);
+         }
+       else
+         
+         
+         {
+         System.out.println("low balance");
+         System.out.println("available balance":+this . amount);
+         }
+       }
+ synchronize void deposit(int amount)
+   
+{
+   this.amount+=amount;
+   System.out.println("deposit completed");
+   System.out.println("available bslance:"+this.amount);
+   notify();
+   
+   class deposit extends thread
+     {
+     Account =a;
+     
+     Deposit (Account);
+     {
+       this.a=a;
+       }
+     public void run()
+       {
+       a.deposit(10000);
+    }
+     }
+   class withdraw extends thread
+     {
+     Account=a;
+   wthdraw(account a)
+     
+   {
+     this.a=a;
+     }
+     public void run()
+       {
+       a.withdraw(25000);
+      }
+     }
+       
+     
+     
+     
+     
+     
+   
+   
